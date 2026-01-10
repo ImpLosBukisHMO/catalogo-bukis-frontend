deefe57895c79fa368a0424fd4977ba514a53197
jafflores <jorgeantoniofloreshdez@hotmail.com>
2026-01-10 13:55:08 -0700

    footer design change

--- catalogo-frontend/src/components/elements/Footer.tsx
+++ catalogo-frontend/src/components/elements/Footer.tsx
@@ -11,24 +11,58 @@ const Footer = (props: { style?: React.CSSProperties }) => {
     return (
         <footer className="pt-3 main-footer" style={props.style}>
             {/* 1st. division */}
-            <div className="content has-text-centered has-text-white">
-                <div className='is-flex-start'>
-                    <div className=''>
-                        <p className='mt-5'>Teléfono: (+52) 662 262 9875</p>
+            <div className="content has-text-white py-2">
+                <div className="columns is-mobile is-centered is-vcentered is-variable is-8 px-6">
+                    
+                    <div className="column is-narrow ml-6">
+                        <p className="has-text-grey-light" style={{ fontSize: '0.90rem' }}>Teléfono:</p>
+                        <a style={{ fontSize: '0.90rem' }} href="tel:+526622629875">
+                            (+52) 662 262 9875
+                        </a>
                     </div>
-                    <div>
-                        <p className='mt-5'>Encuéntranos en:</p>
-                        <p style={{ fontSize: '0.85rem' }}>Blvd. Solidaridad 118 A, Raquet Club II, 83200 Hermosillo, Sonora, México.</p>
+
+                    <div className="column is-narrow">
+                        <p className="has-text-grey-light" style={{ fontSize: '0.90rem' }}>Encuéntranos en:</p>
+                        <p style={{ fontSize: '0.85rem' }}>
+                            Blvd. Solidaridad 118 A, Raquet Club II, 83200 Hermosillo, Sonora, México.
+                        </p>
                     </div>
-                    <div>
-                        <p className='mt-5'>Visita nuestras redes sociales:</p>
-                        <div className='is-flex align-items-center is-justify-content-center'>
-                            <FaFacebook className="social-network-icon" size={36} style={{ marginRight: 12, cursor: 'pointer' }} onClick={() => window.open('https://www.facebook.com/profile.php?id=100063705263484', '_blank')} />
-                            <FaInstagram className="social-network-icon" size={36} style={{ marginLeft: 12, cursor: 'pointer' }} onClick={() => window.open('https://www.instagram.com/bukisimportaciones/', '_blank')} />
+
+
+                    <div className="column has-text-right pr-6 ">
+
+                        <div className="is-flex is-justify-content-flex-end is-align-items-center ">
+                            <p className="mr-3 mb-0 has-text-grey-light" style={{ lineHeight: '1', fontSize: '0.90rem' }}>
+                                Redes sociales:
+                            </p>
+                            <FaFacebook
+                                className="social-network-icon"
+                                size={32}
+                                style={{ marginRight: 10, cursor: 'pointer' }}
+                                onClick={() =>
+                                    window.open(
+                                        'https://www.facebook.com/profile.php?id=100063705263484',
+                                        '_blank'
+                                    )
+                                }
+                            />
+                            <FaInstagram
+                                className="social-network-icon"
+                                size={32}
+                                style={{ cursor: 'pointer' }}
+                                onClick={() =>
+                                    window.open(
+                                        'https://www.instagram.com/bukisimportaciones/',
+                                        '_blank'
+                                    )
+                                }
+                            />
                         </div>
+
                     </div>
+
                 </div>
-            </div>
+            </div> 
 
             {/* 2nd. division */}
             <div className='py-3 is-flex align-items-center is-justify-content-center main-footer-2' style={{width:'100%'}}>
