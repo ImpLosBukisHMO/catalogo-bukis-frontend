e23488c20d909afc292a3b6be15f51c9c5a116ab
Nick-2455 <nicolaspacheco2455@gmail.com>
2026-01-20 19:04:32 -0700

    wip(worker): layout estable con sidebar y rutas

--- catalogo-frontend/src/router.tsx
+++ catalogo-frontend/src/router.tsx
@@ -5,23 +5,30 @@ import ProfilePage from "./components/pages/ProfilePage";
 import CarritoPage from "./components/pages/CarritoPage";
 import LoginPage from "./components/pages/LogInPage";
 import SignUpPage from "./components/pages/SignUpPage";
-import ProductoPage from "./components/pages/ProductoPage";
 import NotFoundPage from "./components/pages/NotFoundPage";
+
+// Worker
+import WorkerLayout from "./components/elements/WorkerLayout";
+import WorkerDashboardPage from "./components/pages/WorkerDashboardPage";
+import WorkerOrdersPage from "./components/pages/WorkerOrdersPage";
+import WorkerProductsPage from "./components/pages/WorkerProductsPage";
 
 export default function AppRouter() {
   return (
     <Routes>
-      {/* Públicas */}
+      {/* Public */}
       <Route path="/" element={<Home />} />
+      <Route path="/perfil" element={<ProfilePage />} />
+      <Route path="/carrito" element={<CarritoPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
 
-      {/* Usuario */}
-      <Route path="/perfil" element={<ProfilePage />} />
-      <Route path="/carrito" element={<CarritoPage />} />
-
-      {/* Producto */}
-      <Route path="/producto/:id" element={<ProductoPage />} />
+      {/* Worker (layout + sidebar) */}
+      <Route path="/worker" element={<WorkerLayout />}>
+        <Route index element={<WorkerDashboardPage />} />
+        <Route path="orders" element={<WorkerOrdersPage />} />
+        <Route path="products" element={<WorkerProductsPage />} />
+      </Route>
 
       {/* Catch all */}
       <Route path="*" element={<NotFoundPage />} />
