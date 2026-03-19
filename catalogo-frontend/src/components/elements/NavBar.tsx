3c8f71533dbf6d00638d857143c42425658c4b1e
UzielMedina22 <uziel.jonathan22@gmail.com>
2026-03-18 19:21:22 -0700

    Fix. Logout - Update 03/18/2026

--- catalogo-frontend/src/components/elements/NavBar.tsx
+++ catalogo-frontend/src/components/elements/NavBar.tsx
@@ -88,7 +88,7 @@ const NavBar = ({navBarQuery}: NavBarProps) => {
                         <Heart size={iconSize} />
                         <p className={pathname === "/favoritos" ? "is-underlined txt-white" : "txt-white"}>Favoritos</p>
                     </Link>
-<Link className="navbar-item main-nav is-flex is-justify-content-center" to="/carrito" style={{ color: "white" }}>
+                    <Link className="navbar-item main-nav is-flex is-justify-content-center" to="/carrito" style={{ color: "white" }}>
                         <ShoppingCart size={iconSize} />
                         <p className={pathname === "/carrito" ? "is-underlined txt-white" : "txt-white"}>Carrito</p>
                     </Link>
