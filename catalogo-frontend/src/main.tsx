80a3f61003016d63a98381c24f9434e368660db7
Nick-2455 <nicolaspacheco2455@gmail.com>
2026-05-30 19:12:55 -0700

    fix: import Bulma CSS in main.tsx

--- catalogo-frontend/src/main.tsx
+++ catalogo-frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "bulma/css/bulma.min.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 import Home from "./components/pages/Home.tsx";
