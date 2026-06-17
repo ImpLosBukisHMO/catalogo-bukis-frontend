c527774f64c4681386cc419f07a70655ecf5c3b3
jafflores <jorgeantoniofloreshdez@hotmail.com>
2026-06-17 02:26:32 -0700

    fix(auth): suppress react-refresh lint error on AuthContext export

---
+++ catalogo-frontend/src/context/AuthContext.tsx
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+
+export type AuthState = {
+    isLoggedIn: boolean;
+    isStaff: boolean;
+    isLoading: boolean;
+    refresh: () => Promise<void>;
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext<AuthState>({
+    isLoggedIn: false,
+    isStaff: false,
+    isLoading: true,
+    refresh: async () => {},
+});
