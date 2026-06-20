6f1c06b149622fbc4c50955b33868ecd72676a3c
Nick-2455 <nicolaspacheco2455@gmail.com>
2026-06-19 19:47:26 -0700

    chore(frontend): add global query client provider

--- catalogo-frontend/src/main.tsx
+++ catalogo-frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import { queryClient } from "./lib/queryClient.ts";
 
 import Home from "./components/pages/Home.tsx";
 import SignUpPage from "./components/pages/SignUpPage.tsx";
@@ -61,6 +64,9 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
   </StrictMode>
 );
